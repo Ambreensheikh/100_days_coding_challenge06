36c764153dfa7f88780878ed85404d9f9e2c6177
Ambreen Sheikh <ambreensheikh900@gmail.com>
2024-03-25 15:59:40 +0500

    first commit

--- 63_Shape_shifter/app.js
+++ 63_Shape_shifter/app.js
@@ -1,1 +1,12 @@
 "use strict";
+let circle = {
+    kind: "Circle",
+    radius: 7
+};
+let Rectangle = {
+    kind: "Rectangle",
+    width: 24,
+    length: 12
+};
+console.log(circle);
+console.log(Rectangle);
--- 63_Shape_shifter/app.ts
+++ 63_Shape_shifter/app.ts
@@ -1,18 +1,20 @@
 
  // making a custome type alias for shapes
- type Kind = {
-   Kind : "circle" | "Rectangle";
+ type Shape = {
+   kind :  "Circle" | "Rectangle";
    radius? : number;   //for circle
    width? : number;    //for rectangle
    length? : number;   //for rectangle
 
  };
- let circle : shape ={
-     Kind  : "circle",
+ let circle: Shape = {
+     kind  : "Circle",
      radius : 7 
  };
- let Rectangle : shape = {
-    Kind : "Rectangle",
+ let Rectangle: Shape= {
+    kind : "Rectangle",
     width : 24,
     length : 12
  };
+  console.log(circle);
+  console.log(Rectangle)
